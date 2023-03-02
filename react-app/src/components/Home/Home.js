2977c0560ad49bab30e53603a0f58ff8d75cbf50
kevin <kevingao52997@gmail.com>
2023-03-02 12:24:59 -0800

    tweaked styling for friends list

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -6,9 +6,6 @@ import './Home.css';
 export default function Home() {
   return (
     <main>
-      <div className='header'>
-        <h1>Mork Fuh Def</h1>
-      </div>
       <div className='home-body'>
         <div className='games'></div>
         <div className='friends-list-wrapper'>
