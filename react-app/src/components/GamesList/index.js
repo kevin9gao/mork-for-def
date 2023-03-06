0f6388186e86e9662f9492b0d65d05fd6ca89f94
kevin <kevingao52997@gmail.com>
2023-03-06 15:26:11 -0800

    game list component basic styling

--- react-app/src/components/GamesList/index.js
+++ react-app/src/components/GamesList/index.js
@@ -1,12 +1,76 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import './GamesList.css';
 
 export default function GamesList() {
   const dispatch = useDispatch();
+  const games = useSelector(state => state.games);
+  const gamesArray = games ? Object.values(games) : null;
+  console.log(gamesArray);
+
+  // TODO: sort games so that finished games are at the bottom
+  const myGames = gamesArray?.map(game => {
+    // if (!game.includes('active')) return null;
+
+    if (game.active) {
+      return (
+        <li>
+          <div className="games-list-ele">
+            <div>
+              <span>{game.name}</span>
+            </div>
+            <div>
+              <span>Phase</span>
+              <span>{game.phase}</span>
+            </div>
+            <div>
+              <span>Players</span>
+              <span>{game.num_players}</span>
+            </div>
+            <div>
+              <NavLink to={`/games/${game.id}`}>Play</NavLink>
+            </div>
+          </div>
+        </li>
+      );
+    } else {
+      return (
+        <li>
+          <div className="games-list-ele">
+            <div>
+              <span>{game.name}</span>
+            </div>
+            <div>
+              <span>Phase</span>
+              <span>Concluded</span>
+            </div>
+            <div>
+              <span>Players</span>
+              <span>{game.num_players}</span>
+            </div>
+            <div>
+              <NavLink to={`/games/${game.id}`}>View</NavLink>
+            </div>
+          </div>
+        </li>
+      );
+    }
+  });
 
   return (
     <div className="games-list-wrapper">
       <h2>My Games</h2>
+      <div id="my-games">
+        {myGames && (
+          <ul id="my-games-ul">
+            {myGames}
+          </ul>
+        )}
+        {!myGames && (
+          <h3>You do not have any active games.</h3>
+        )}
+      </div>
     </div>
   );
 }
--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUsersGames } from '../../store/games';
 import FriendsList from '../FriendsList';
+import GamesList from '../GamesList';
 import GameState from '../GameState/GameState';
 import NewGame from '../NewGame';
 import './Home.css';
 
 export default function Home() {
+  const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUsersGames(sessionUser.id));
+  }, []);
+
   return (
     <main>
       <div className='home-body'>
-        <div className='games'></div>
+        <div className='games'>
+          <GamesList />
+        </div>
         <div className='friends-list-wrapper'>
           <NewGame />
           <FriendsList />
